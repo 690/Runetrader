e4f01b4af347a3c711a75942b48394b1a01835bc
frederik Gram <frederikxyz@hotmail.com>
2018-09-30 03:12:21 +0200

    updated random movement in move()

--- tools/realistic_mouse.py
+++ tools/realistic_mouse.py
@@ -1,16 +1,45 @@
 import random
 import time
 import pyautogui
+import scipy
 
+from scipy import interpolate
 from config import SMALL_DELAY_RANGE
 from tools.utils import random_position
 
 
-def random_path(x1, y1, x2, y2):
-    """ Create a random mouse movement path from one coordinate to another """
+def move(target_x, target_y):
+    """ Move mouse according to random bezier curve
+    credit : DJV : https://stackoverflow.com/a/44666798 """
 
-    return x2, y2
+    current_x, current_y = pyautogui.position()
 
+    control_points = 3
+
+    x = scipy.linspace(current_x, target_x, control_points, dtype="int")
+    y = scipy.linspace(current_y, target_y, control_points, dtype="int")
+
+    seed = random.randint(5, 55)
+
+    xr = scipy.random.randint(-seed, seed, size=control_points)
+    yr = scipy.random.randint(-seed, seed, size=control_points)
+
+    xr[0] = yr[0] = xr[-1] = yr[-1] = 0
+    x += xr
+    y += yr
+
+    degree = 3 if control_points > 3 else control_points - 1  # Degree of b-spline. 3 is recommended.
+    tck, u = scipy.interpolate.splprep([x, y], k=degree)
+    u = scipy.linspace(0, 1, num=max(pyautogui.size()))
+    points = scipy.interpolate.splev(u, tck)
+
+    duration = 0.0001
+    timeout = duration / len(points[0])
+
+    for point in zip(*(i.astype(int) for i in points)):
+        pyautogui.moveTo(*point)
+        if random.randint(1, 10) == 2:
+            time.sleep(timeout)
 
 def click(x, y):
     """ Click a point with a random delay """
@@ -19,25 +48,8 @@ def click(x, y):
     pyautogui.click(x, y)
 
 
-def move(target_x, target_y):
-    """ Move the mouse to a given point, using a random path and delays """
-
-    time.sleep(random.uniform(*SMALL_DELAY_RANGE))
-
-    current_x, current_y = pyautogui.position()
-    path = random_path(current_x, current_y, target_x, target_y)
-
-    distance = int(((target_x - current_x) ** 2 + (target_y - current_y) ** 2) ** 0.5)
-    pyautogui.moveTo(target_x, target_y, (distance * random.random() / 2000) + 0.5, pyautogui.easeInQuad)  # https://github.com/lukegarbott
-
-
 def all_in_one(x, y, z, w):
     random_x, random_y = random_position(x, y, x + z, y + w)
     move(random_x, random_y)
     click(*pyautogui.position())
 
-
-if __name__ == "__main__":
-    """ Run unit tests """
-
-    move(500, 500)
